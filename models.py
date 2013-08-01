76724271f38d4e6464146dc46f3de6ddbcc65974
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-07-31 22:43:18 -0400

    Added user Schema and cleaned up other schemas

--- models.py
+++ models.py
@@ -18,7 +18,14 @@ class Image(db.Document):
 
 class ClothingFeature(db.EmbeddedDocument):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
-    key = db.StringField(max_length=255, required=True)
-    value = db.StringField(required=True)
+    value = db.DictField(required=True)
 
+class User(db.Document):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=False)
+    username = db.StringField(max_length=255, required=True)
+    password = db.StringField(max_length=255, required=True)
+    email = db.EmailField(max_length=255, required=True)
+    is_staff = db.BooleanField(default=False, required=True)
+    associated_company = db.StringField(default="Spool" required=True)
+    
     
