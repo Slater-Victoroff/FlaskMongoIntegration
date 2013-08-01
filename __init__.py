76b72a1b8d28295ec1016efcc9789d1a20b2efc3
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-07-31 22:22:34 -0400

    Basic Mongo integration added

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {"DB": "Mongo_Integration_Demo"}
+app.config["SECRET_KEY"] = "InsecurePassword"
+
+db = MongoEngine(app)
 
 if __name__ == '__main__':
     app.run()
