0db49bd746739f3c3004ccbe4618e42085f75f49
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-07-31 23:26:12 -0400

    added template placeholder and basic layout

--- __init__.py
+++ __init__.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {"DB": "Mongo_Integration_Demo"}
 app.config["SECRET_KEY"] = "InsecurePassword"
 
+def register_blueprints(app):
+    from MongoIntegration.views import clothes
+    app.register_blueprint(clothes)
+
+register_blueprints(app)
 db = MongoEngine(app)
 
 if __name__ == '__main__':
